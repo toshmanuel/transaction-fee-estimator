be5dd8fff720c17f75a50e3ff10d32f33a0027a3
toshmanuel <oladimejiomotosho5@gmail.com>
2022-06-12 11:04:50 +0100

    handled error for block confirmation that is not within range

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# transaction-fee-estimator
-Estimate Bitcoin Transaction Fee
+# Bitcoin Transaction Fee Estimator
+Knowing the exact amount to pay as bitcoin transaction fee can be daunting since it is not fixed and most times, it is based on the transaction size, number of unconfirmed transactions in the mempool, and how fast you want your transaction to be confirmed. Bitcoin fee estimator is a python application that allows users to have an idea of how much to pay for their bitcoin transaction to avoid overpaying or underpaying. This application provides users with APIs for use with any GUI or other backend applications.
+
--- api/v1/fee_rate_per_confirmation.py
+++ api/v1/fee_rate_per_confirmation.py
@@ -3,21 +3,20 @@ from requests import Session
 from api.v1.fee import get_fee
 
 def calculate_fee_base_on_block_confirmation_target(num_of_inputs, num_of_outputs, tx_type, target_block):
-    
     if target_block < 1 or target_block > 1008:
         raise ValueError("Target block must be between 1 and 1008")
     result = Session().get('https://blockstream.info/testnet/api/fee-estimates')
 
-    if target_block >= 25 and target_block < 144:
+    if 25 <= target_block < 144:
         fee_rate = result.json()[str(25)]
-    elif target_block >= 144 and target_block < 504:
+    elif 144 <= target_block < 504:
         fee_rate = result.json()[str(144)]
-    elif target_block >= 504 and target_block < 1008:
+    elif 504 <= target_block < 1008:
         fee_rate = result.json()[str(504)]
     else:
         fee_rate = result.json()[str(target_block)]
-    
-    fee = get_fee(num_of_inputs,num_of_outputs,tx_type,)
+
+    fee = get_fee(num_of_inputs, num_of_outputs, tx_type, )
 
     return fee_rate * fee
     
